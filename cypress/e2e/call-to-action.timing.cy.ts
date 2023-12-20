3e9f82b728588051eeb37f2587b0ff623b12f37f
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2023-12-20 11:10:01 +0200

    fix setTimeout tests

--- cypress/e2e/call-to-action.timing.cy.ts
+++ cypress/e2e/call-to-action.timing.cy.ts
@@ -1,44 +1,48 @@
 import {getOverlayElement, getPlayButtonElement, getPopupElement, loadPlayerAndSetMedia} from './utils/env';
 
 const TITLE = 'title';
-const SET_TIMEOUT_MARGIN = 100;
+const EXECUTION_TIME_MARGIN = 100;
 
-const expectElementExistsAt = (pluginConfig: object, time: number, getElement: () => any) => {
+const expectElementExistsAt = (pluginConfig: object, expectedStartTime: number, getElement: () => any) => {
   loadPlayerAndSetMedia(pluginConfig).then(kalturaPlayer => {
     getPlayButtonElement().should('exist').click({force: true});
     getPlayButtonElement()
       .should('not.exist')
       .then(() => getElement().should('exist'))
-      .then(() => expect(kalturaPlayer.currentTime).to.be.at.least(time));
+      .then(() => expect(kalturaPlayer.currentTime).to.be.at.least(expectedStartTime));
   });
 };
 
-const expectElementDoesntExistAfter = (pluginConfig: object, expectedDurationInSeconds: number, getElement: () => any) => {
-  loadPlayerAndSetMedia(pluginConfig).then(() => {
-    let messageStartTime = 0;
+const expectElementDoesntExistAfter = (pluginConfig: object, expectedStartTime: number, expectedDuration: number, getElement: () => any) => {
+  let startTimeDelta = 0;
+  let timestampOnStart = 0;
+  loadPlayerAndSetMedia(pluginConfig).then(kalturaPlayer => {
     getPlayButtonElement().should('exist').click({force: true});
     getPlayButtonElement()
       .should('not.exist')
-      .then(() => getElement().should('exist'))
       .then(() => {
-        messageStartTime = Date.now();
+        return getElement().should('exist');
+      })
+      .then(() => {
+        timestampOnStart = Date.now();
+        startTimeDelta = kalturaPlayer.currentTime - expectedStartTime;
         return getElement().should('not.exist');
       })
-      .then(() => expect(Date.now() - messageStartTime + SET_TIMEOUT_MARGIN).to.be.at.least(expectedDurationInSeconds * 1000));
+      .then(() => expect(Date.now() - timestampOnStart + EXECUTION_TIME_MARGIN).to.be.at.least(1000 * (expectedDuration - startTimeDelta)));
   });
 };
 
-const expectPopupExistsAt = (pluginConfig: object, time: number) => {
-  expectElementExistsAt(pluginConfig, time, getPopupElement);
+const expectPopupExistsAt = (pluginConfig: object, expectedStartTime: number) => {
+  expectElementExistsAt(pluginConfig, expectedStartTime, getPopupElement);
 };
-const expectPopupDoesntExistAfter = (pluginConfig: object, time: number) => {
-  expectElementDoesntExistAfter(pluginConfig, time, getPopupElement);
+const expectPopupDoesntExistAfter = (pluginConfig: object, expectedStartTime: number, expectedDuration: number) => {
+  expectElementDoesntExistAfter(pluginConfig, expectedStartTime, expectedDuration, getPopupElement);
 };
-const expectOverlayExistsAt = (pluginConfig: object, time: number) => {
-  expectElementExistsAt(pluginConfig, time, getOverlayElement);
+const expectOverlayExistsAt = (pluginConfig: object, expectedStartTime: number) => {
+  expectElementExistsAt(pluginConfig, expectedStartTime, getOverlayElement);
 };
-const expectOverlayDoesntExistAfter = (pluginConfig: object, time: number) => {
-  expectElementDoesntExistAfter(pluginConfig, time, getOverlayElement);
+const expectOverlayDoesntExistAfter = (pluginConfig: object, expectedStartTime: number, expectedDuration: number) => {
+  expectElementDoesntExistAfter(pluginConfig, expectedStartTime, expectedDuration, getOverlayElement);
 };
 
 describe('message timing', () => {
@@ -80,6 +84,7 @@ describe('message timing', () => {
               }
             ]
           },
+          0,
           2
         );
         expectOverlayDoesntExistAfter(
@@ -91,19 +96,173 @@ describe('message timing', () => {
               }
             ]
           },
+          0,
           2
         );
       });
     });
-    // describe('show on end', () => {
-
-    // });
-    // describe('time from start', () => {
-
-    // });
-    // describe('time from end', () => {
-
-    // });
+    describe('show on end', () => {
+      it('should display message on playback end', () => {
+        expectPopupExistsAt(
+          {
+            messages: [
+              {
+                showToast: true,
+                title: TITLE,
+                timing: {showOnEnd: true}
+              }
+            ]
+          },
+          4
+        );
+        expectOverlayExistsAt(
+          {
+            messages: [
+              {
+                title: TITLE,
+                timing: {showOnEnd: true}
+              }
+            ]
+          },
+          4
+        );
+      });
+      it('should hide message after duration', () => {
+        expectPopupDoesntExistAfter(
+          {
+            messages: [
+              {
+                showToast: true,
+                title: TITLE,
+                timing: {showOnEnd: true, duration: 2}
+              }
+            ]
+          },
+          4,
+          2
+        );
+        expectOverlayDoesntExistAfter(
+          {
+            messages: [
+              {
+                title: TITLE,
+                timing: {showOnEnd: true, duration: 2}
+              }
+            ]
+          },
+          4,
+          2
+        );
+      });
+    });
+    describe('time from start', () => {
+      it('should display message on time from start', () => {
+        expectPopupExistsAt(
+          {
+            messages: [
+              {
+                showToast: true,
+                title: TITLE,
+                timing: {timeFromStart: 1}
+              }
+            ]
+          },
+          1
+        );
+        expectOverlayExistsAt(
+          {
+            messages: [
+              {
+                title: TITLE,
+                timing: {timeFromStart: 1}
+              }
+            ]
+          },
+          1
+        );
+      });
+      it('should hide message after duration', () => {
+        expectPopupDoesntExistAfter(
+          {
+            messages: [
+              {
+                showToast: true,
+                title: TITLE,
+                timing: {timeFromStart: 1, duration: 2}
+              }
+            ]
+          },
+          1,
+          2
+        );
+        expectOverlayDoesntExistAfter(
+          {
+            messages: [
+              {
+                title: TITLE,
+                timing: {timeFromStart: 1, duration: 2}
+              }
+            ]
+          },
+          1,
+          2
+        );
+      });
+    });
+    describe('time from end', () => {
+      it('should display message on time from end', () => {
+        expectPopupExistsAt(
+          {
+            messages: [
+              {
+                showToast: true,
+                title: TITLE,
+                timing: {timeFromEnd: 2}
+              }
+            ]
+          },
+          2
+        );
+        expectOverlayExistsAt(
+          {
+            messages: [
+              {
+                title: TITLE,
+                timing: {timeFromEnd: 2}
+              }
+            ]
+          },
+          2
+        );
+      });
+      it('should hide message after duration', () => {
+        expectPopupDoesntExistAfter(
+          {
+            messages: [
+              {
+                showToast: true,
+                title: TITLE,
+                timing: {timeFromEnd: 3, duration: 2}
+              }
+            ]
+          },
+          1,
+          2
+        );
+        expectOverlayDoesntExistAfter(
+          {
+            messages: [
+              {
+                title: TITLE,
+                timing: {timeFromEnd: 3, duration: 2}
+              }
+            ]
+          },
+          1,
+          2
+        );
+      });
+    });
   });
   //describe('seeked event');
   // message ended normally
