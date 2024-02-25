5f014696be398e6f5988961fe56ad63e826b2f52
SivanA-Kaltura <88330203+SivanA-Kaltura@users.noreply.github.com>
2024-02-25 11:59:14 +0200

    update cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,9 +6,7 @@ export default defineConfig({
   defaultCommandTimeout: 30000,
   fileServerFolder: 'cypress/public',
   e2e: {
-    watchForFileChanges: false,
-    setupNodeEvents(on, config) {
-      require('cypress-terminal-report/src/installLogsPrinter')(on);
-    }
+    supportFile: false,
+    watchForFileChanges: false
   }
 });
