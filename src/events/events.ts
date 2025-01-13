920abf6c0bfcda3b0822df68c0326cd7ebf77018
quantumInverter <blackhand07@gmail.com>
2025-01-13 16:02:08 +0200

    fix(FEC-14011): Slide and CTA Analytics

--- src/events/events.ts
+++ src/events/events.ts
@@ -1,3 +1,4 @@
 export const CallToActionEvents = {
-  CALL_TO_ACTION_BUTTON_CLICK: 'call_to_action_button_click'
+  CALL_TO_ACTION_BUTTON_CLICK: 'call_to_action_button_click',
+  CALL_TO_ACTION_DISPLAYED: 'call_to_action_displayed'
 };
